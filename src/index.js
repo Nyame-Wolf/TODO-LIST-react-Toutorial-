632a7bbdab5c1b234ba7c3d47adcee47818e5f02
Nyame-Wolf <mumenyam@gmail.com>
2022-08-08 19:20:31 +0300

    Add a component to TodoContainer
    Add state array and use map method to render array items title.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import ReactDOM from 'react-dom/client';
+import TodoContainer from "./components/TodoContainer";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-   <h1>Hello from Create React App</h1>
+  <TodoContainer />
   </React.StrictMode>, 
 )
 
