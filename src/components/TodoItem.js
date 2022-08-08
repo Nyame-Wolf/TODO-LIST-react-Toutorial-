8e4f8fd967f21255a2910755f9f045ad8247a382
Nyame-Wolf <mumenyam@gmail.com>
2022-08-08 20:00:39 +0300

    revert back TodoItem component to class based

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-function TodoItem (props){
-        return(
-            <li>{props.todo.title}</li>
+class TodoItem extends React.Component{
+    render(){
+       return(
+            <li>{this.props.todo.title}</li>
         )
+    }
+       
     }
 
 export default TodoItem
