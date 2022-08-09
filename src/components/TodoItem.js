19a474221329976c503a10b7b6bea0df0d0b697a
Nyame-Wolf <mumenyam@gmail.com>
2022-08-09 09:34:50 +0300

    Style React App with CSS Modules

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,13 @@
 import React from "react";
+import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component{
     render(){
        return(
-            <li>
+            <li className={styles.item}>
                 <input type="checkbox"
                  checked={this.props.todo.completed} 
+                 className={styles.checkbox}
                  onChange={() => this.props.handleChangeProps(this.props.todo.id)}
                  /> 
                  <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>Delete</button>
