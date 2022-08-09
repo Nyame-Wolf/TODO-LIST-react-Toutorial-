3a349980d47e91dbfa74749bfa9c0aad76b05eac
Nyame-Wolf <mumenyam@gmail.com>
2022-08-09 09:29:31 +0300

    add a css stylesheet and import it in index.js file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from 'react-dom/client';
 import TodoContainer from "./components/TodoContainer";
 
+import "./App.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
